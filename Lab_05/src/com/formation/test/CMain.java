026c09d1f8eb2e57cb2da348f1626340a2775535
dev <amour-youcef@hotmail.com>
2020-05-11 03:14:59 +0200

    11052020

--- Lab_05/src/com/formation/test/CMain.java
+++ Lab_05/src/com/formation/test/CMain.java
@@ -1,5 +1,11 @@
 package com.formation.test;
 
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import com.formation.domaine.CCamion;
 import com.formation.domaine.CVehicule;
 import com.formation.domaine.CVoiture;
@@ -10,23 +16,46 @@ public class CMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		
 
-		CVehicule[] listeVehicule;
-		listeVehicule = new CVehicule[10];
+		List<CVehicule> listeVehicule;
+		listeVehicule = new ArrayList<CVehicule>();
 
-		listeVehicule[0] = new CVoiture("Peugeot", 100);
-		listeVehicule[1] = new CCamion("Iveco", 200);
+		listeVehicule.add(new CVoiture("Peugeot", 100));
+		listeVehicule.add(new CCamion("Iveco", 200));
+		
+		Map<String, CVehicule> mapVehicule = new Hashtable<String, CVehicule>();
+		
+		mapVehicule.put("Peugeot", new CVoiture("Peugeot", 100));
+		mapVehicule.put("Iveco", new CVoiture("Iveco", 200));
 
-		for (int i = 0; i < listeVehicule.length && listeVehicule[i] != null; i++) {
+		// Etape 1
 
-			if(listeVehicule[i] instanceof CVoiture){
-				((CVoiture)listeVehicule[i]).rouler();
-			}else if(listeVehicule[i] instanceof CCamion ){
-				((CCamion)listeVehicule[i]).rouler();
+		for (int i = 0; i < listeVehicule.size()
+				&& listeVehicule.get(i) != null; i++) {
+			CVehicule v = listeVehicule.get(i);
+			if (v instanceof CVoiture) {
+				((CVoiture) v).rouler();
+			} else if (v instanceof CCamion) {
+				((CCamion) v).rouler();
 			}
-
-
 		}
 
+		// Etape 2
+		Iterator<CVehicule> iterator = listeVehicule.iterator();
+		CVehicule v;
+		while (iterator.hasNext()) {
+			v = iterator.next();
+			if (v instanceof CVoiture) {
+				((CVoiture) v).rouler();
+			} else if (v instanceof CCamion) {
+				((CCamion) v).rouler();
+			}
+		}
+		
+		// Etape 3
+		 ((CVehicule)mapVehicule.get("Iveco")).afficher();
+		
 	}
 }
